0c7d4589642662149843a5f4e305a2634251dfb6
ilbrigz <ilogirb23@yahoo.com>
2019-06-24 22:31:50 +0800

    Updates

---
+++ precache-manifest.4e8c92ba22746bd02ede48b312b70e91.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8aee10d0bf795fb047e869d218362fb3",
+    "url": "/pdftest/index.html"
+  },
+  {
+    "revision": "82d14f706df912d6d9dc",
+    "url": "/pdftest/static/css/main.dffe6a47.chunk.css"
+  },
+  {
+    "revision": "47f817f8032425702105",
+    "url": "/pdftest/static/js/2.2406de80.chunk.js"
+  },
+  {
+    "revision": "82d14f706df912d6d9dc",
+    "url": "/pdftest/static/js/main.2a11feac.chunk.js"
+  },
+  {
+    "revision": "7005f008d7290d2409bc",
+    "url": "/pdftest/static/js/runtime~main.36a76774.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/pdf-test/precache-manifest.5bef8f7203bc766a153827bbc892b406.js"
+  "/pdftest/precache-manifest.4e8c92ba22746bd02ede48b312b70e91.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/pdf-test/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/pdftest/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
