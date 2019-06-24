da06495b6b8b9bab1fbbc5e331b5601822c4857a
ilbrigz <ilogirb23@yahoo.com>
2019-06-24 22:23:38 +0800

    Updates

---
+++ precache-manifest.790d7e217229a03b75875af53645e150.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c57dd95c8bf7ba277c70203c836e6ec",
+    "url": "/pdf-test/index.html"
+  },
+  {
+    "revision": "9abf39aeec65b9156839",
+    "url": "/pdf-test/static/css/main.dffe6a47.chunk.css"
+  },
+  {
+    "revision": "47f817f8032425702105",
+    "url": "/pdf-test/static/js/2.2406de80.chunk.js"
+  },
+  {
+    "revision": "9abf39aeec65b9156839",
+    "url": "/pdf-test/static/js/main.113871ad.chunk.js"
+  },
+  {
+    "revision": "ee260e1730ca90a8dedc",
+    "url": "/pdf-test/static/js/runtime~main.c7c3eb13.js"
+  }
+]);
