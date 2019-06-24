9ea7b8b52833f4f1a9663e80a0eec3e012b56e82
ilbrigz <ilogirb23@yahoo.com>
2019-06-24 22:33:43 +0800

    Updates

---
+++ precache-manifest.c361c612745c0bd4e552bb1f05980fc4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d10c0259cbb06f76628ee77d7d74792",
+    "url": "/test-question-pdfmake/index.html"
+  },
+  {
+    "revision": "afd7dc1808cfc4ba11a2",
+    "url": "/test-question-pdfmake/static/css/main.dffe6a47.chunk.css"
+  },
+  {
+    "revision": "47f817f8032425702105",
+    "url": "/test-question-pdfmake/static/js/2.2406de80.chunk.js"
+  },
+  {
+    "revision": "afd7dc1808cfc4ba11a2",
+    "url": "/test-question-pdfmake/static/js/main.56dca98e.chunk.js"
+  },
+  {
+    "revision": "35dfa3a6476b41f64aec",
+    "url": "/test-question-pdfmake/static/js/runtime~main.f505e1e4.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/pdftest/precache-manifest.4e8c92ba22746bd02ede48b312b70e91.js"
+  "/test-question-pdfmake/precache-manifest.c361c612745c0bd4e552bb1f05980fc4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/pdftest/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/test-question-pdfmake/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
