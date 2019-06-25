fc6ed9e6f2b62da03d65c2bebeed4b842c252b5a
ilbrigz <ilogirb23@yahoo.com>
2019-06-25 23:26:32 +0800

    Updates

---
+++ precache-manifest.b5fe8ba76a5447ece2373a46fcd4666b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0590a02a0e885a6d6cf283a90dd1e1c6",
+    "url": "/test-question-pdfmake/index.html"
+  },
+  {
+    "revision": "a2c2f54c9d75d0d2355e",
+    "url": "/test-question-pdfmake/static/css/main.dc201493.chunk.css"
+  },
+  {
+    "revision": "22911a3330ce8186f35e",
+    "url": "/test-question-pdfmake/static/js/2.497cdfd1.chunk.js"
+  },
+  {
+    "revision": "a2c2f54c9d75d0d2355e",
+    "url": "/test-question-pdfmake/static/js/main.20e88a75.chunk.js"
+  },
+  {
+    "revision": "35dfa3a6476b41f64aec",
+    "url": "/test-question-pdfmake/static/js/runtime~main.f505e1e4.js"
+  }
+]);
